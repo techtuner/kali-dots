81bcecedc6f9c0d1ac381aa30e31f03c5e00ac89
techtuner <techtuner284@gmail.com>
2023-01-27 11:38:00 +0530

    Add Install Script

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ sudo apt-get install kali-tweaks tmux powerline xclip lolcat name-that-hash -y
 cp ./.vimrc ~
 
 cp greeting.txt ~
-
+cp -r wallpaper ~/Pictures/
 echo "lolcat ~/greeting.txt" >> ~/.zshrc
+
+
+sudo cp -r ./fonts/Monaco /usr/share/fonts/truetype
+sudo cp -r ./fonts/MonoLisa /usr/share/fonts/truetype
