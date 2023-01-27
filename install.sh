5337e5659b8a49482ed4766e7908df8f9af3fb09
techtuner <techtuner284@gmail.com>
2023-01-27 11:23:32 +0530

    Add Install Script

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+sudo apt-get install kali-tweaks tmux powerline xclip lolcat name-that-hash -y
+
+cp ./.vimrc ~
+
+cp greeting.txt ~
+
+echo "lolcat ~/greeting.txt" >> ~/.zshrc
