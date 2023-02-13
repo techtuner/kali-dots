9aeaea86b55ed6eea11c8f28f632b7876c1cf7a1
techtuner <techtuner284@gmail.com>
2023-02-13 13:48:33 +0530

    Updated

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 
-sudo apt-get install name-that-hash python3-pip curl dnsrecon enum4linux feroxbuster gobuster lolcat impacket-scripts nbtscan nikto onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
+sudo apt-get install terminator name-that-hash python3-pip curl dnsrecon enum4linux feroxbuster gobuster lolcat impacket-scripts nbtscan nikto onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
 
 sudo apt install python3-venv -y
 sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git
