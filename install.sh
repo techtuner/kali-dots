7ef240d1c16c38ed95ee7db9c0aceeddc93a521b
techtuner <techtuner284@gmail.com>
2023-02-13 12:30:45 +0530

    Updated

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 
-sudo apt-get install terminator name-that-hash python3-pip curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
+sudo apt-get install name-that-hash python3-pip curl dnsrecon enum4linux feroxbuster gobuster lolcat impacket-scripts nbtscan nikto onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
 
 sudo apt install python3-venv
 sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 cp ./.vimrc ~
-# cp -r ./terminator ~/.config/
 cp -r wallpaper ~/Pictures/
 cp ./tmux/.tmux.conf ~
+cp japanese.txt ~
 
-sudo cp -r ./fonts/Monaco /usr/share/fonts/truetype
-sudo cp -r ./fonts/MonoLisa /usr/share/fonts/truetype
+sudo cp -r ./fonts/Fira-Code /usr/share/fonts/truetype
+echo "lolcat ~/japanese.txt" >> ~/.zshrc
