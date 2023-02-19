0c0d2e356074f79048f606906e238ca2cb3afec3
Santhosh Pai <techtuner284@gmail.com>
2023-02-19 21:42:41 +0530

    Create aliases.zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,7 @@
+alias src="source ~/.zshrc"
+alias python="python3"
+alias pip="pip3"
+alias lg="lazygit" # Works only inside a git folder or can be used to initialize a git folder
+alias e="exit"
+alias dots="cd ~/Downloads/kali-dots && ls"
+alias wallpapers=" gio open ~/Pictures/wallpapers" 
