2eb9da729acb601d102631464be617ec62625133
Santhosh Pai <techtuner284@gmail.com>
2023-02-19 21:58:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 
+# Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 sudo apt-get install bless terminator name-that-hash python3-pip curl dnsrecon enum4linux feroxbuster gobuster lolcat impacket-scripts nbtscan nikto onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
 
 sudo apt install python3-venv -y
@@ -11,6 +14,9 @@ cp ./.vimrc ~
 cp -r wallpaper ~/Pictures/
 cp ./tmux/.tmux.conf ~
 cp banner ~
+cp -r ./zsh ~
 
 sudo cp -r ./fonts/Fira-Code /usr/share/fonts/truetype
 echo "lolcat ~/banner" >> ~/.zshrc
+echo "source ~/zsh/aliases.zsh" >> ~/.zshrc
+source ~/.zshrc
