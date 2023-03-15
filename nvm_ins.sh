9fb34e35efc8f5dbd78d8598ca76962c3f8105ed
Santhosh Pai <techtuner284@gmail.com>
2023-03-15 08:33:05 +0530

    Create nvm_ins.sh

---
+++ nvm_ins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+source ~/.zshrc
+
+
+nvm install --lts
+nvm use 18.15.0
